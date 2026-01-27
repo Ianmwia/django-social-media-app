566e6b462c2344d00d31ccbba0c154e8f3ef402a
Mwia <iansnr10@gmail.com>
2026-01-27 08:01:19 +0300

    create the user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = CloudinaryField('image', null=True, blank=True)
+    bio = models.TextField(_("persons biography"), blank=True)
+
+    def __str__(self):
+        return self.user.username
