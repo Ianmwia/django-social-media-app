589ddd730f79321f55281ca7c5ba3854739afa7b
Mwia <iansnr10@gmail.com>
2026-01-28 18:55:36 +0300

    changing user model from django built in to custom user

--- users/models.py
+++ users/models.py
@@ -1,11 +1,48 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 from django.utils.translation import gettext_lazy as _
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.base_user import BaseUserManager
 
-# Create your models here.
+#custom user
+class CustomUserManager(BaseUserManager):
+    """Manager where email is the unique identifier for authentication."""
+    def create_user(self, username, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_("The Email must be set"))
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(username, email, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser):
+    user_id = models.AutoField(_("user id"), primary_key=True)
+    username = models.CharField(_("username"), max_length=50, unique=True)
+    email = models.EmailField(_('email address'), unique=True)
+
+    date_created = models.DateTimeField(_("date user joined"), auto_now_add=True)
+    is_active = models.BooleanField(default=True, blank=True)
+    is_staff = models.BooleanField(default=False, blank=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+    objects = CustomUserManager()
+    
+    def __str__(self):
+        return self.email
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     image = CloudinaryField('image', null=True, blank=True)
     bio = models.TextField(_("persons biography"), blank=True)
 
